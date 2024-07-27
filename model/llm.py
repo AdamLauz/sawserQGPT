b1cde504315931ff72a07ed919757c0136c2427f
Adam Lauz <adamlauz@gmail.com>
2024-07-27 18:02:41 +0300

    update llm code

--- model/llm.py
+++ model/llm.py
@@ -1,31 +1,41 @@
-from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from optimum.gptq.quantizer import QuantizationConfig
 from pathlib import Path
 
 SAVED_MODEL_PATH = "../flask"
-LLM_PATH = str(Path(SAVED_MODEL_PATH, "LLM.h5"))
-LLM_TOKENIZER_PATH = str(Path(SAVED_MODEL_PATH, "LLM_TOKENIZER.h5"))
+LLM_PATH = str(Path(SAVED_MODEL_PATH, "LLM"))
+LLM_TOKENIZER_PATH = str(Path(SAVED_MODEL_PATH, "LLM_TOKENIZER"))
 
 
 def load_llm():
-    # import LLM
     model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
-    model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                 device_map="auto",
-                                                 trust_remote_code=False,
-                                                 revision="main")
 
-    config = PeftConfig.from_pretrained("shawhin/shawgpt-ft")
-    model = PeftModel.from_pretrained(model, "shawhin/shawgpt-ft")
+    # Create a QuantizationConfig with Exllama disabled
+    quantization_config = QuantizationConfig(
+        disable_exllama=True
+    )
 
-    # load tokenizer
+    # Load model
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        device_map="auto",  # Ensure this is configured for GPU usage if available
+        trust_remote_code=False,
+        revision="main",
+        quantization_config=quantization_config  # Pass the quantization config
+    )
+
+    # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
 
     return model, tokenizer
 
 
 if __name__ == "__main__":
-    # save models locally
+    # Ensure that directories exist
+    Path(LLM_PATH).mkdir(parents=True, exist_ok=True)
+    Path(LLM_TOKENIZER_PATH).mkdir(parents=True, exist_ok=True)
+
+    # Save models locally
     model, tokenizer = load_llm()
     model.save_pretrained(LLM_PATH)
     tokenizer.save_pretrained(LLM_TOKENIZER_PATH)
