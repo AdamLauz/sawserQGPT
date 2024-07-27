09a7ad103258016fe7f9ca101acd515e712cdb0b
Adam Lauz <adamlauz@gmail.com>
2024-07-27 18:30:12 +0300

    disable exllama

--- model/llm.py
+++ model/llm.py
@@ -1,5 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from optimum.gptq.quantizer import QuantizationConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from pathlib import Path
 
 SAVED_MODEL_PATH = "../flask"
@@ -10,10 +9,9 @@ LLM_TOKENIZER_PATH = str(Path(SAVED_MODEL_PATH, "LLM_TOKENIZER"))
 def load_llm():
     model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
 
-    # Create a QuantizationConfig with Exllama disabled
-    quantization_config = QuantizationConfig(
-        disable_exllama=True
-    )
+    # disable exllama to be able to run on CPU
+    config = AutoConfig.from_pretrained(model_name)
+    config.quantization_config["disable_exllama"] = True
 
     # Load model
     model = AutoModelForCausalLM.from_pretrained(
@@ -21,7 +19,7 @@ def load_llm():
         device_map="auto",  # Ensure this is configured for GPU usage if available
         trust_remote_code=False,
         revision="main",
-        quantization_config=quantization_config  # Pass the quantization config
+        config=config  # Pass the config
     )
 
     # Load tokenizer
