443aae90731be55498de6fcbce0c8fbdb126ca76
Adam Lauz <adamlauz@gmail.com>
2024-08-17 17:56:39 +0300

    bring back usage

--- flask/server.py
+++ flask/server.py
@@ -13,7 +13,7 @@ def predict():
     user_query = request.get_json().get("query")
 
     # make a query
-    answer = "dummy answer" #sawserq_gpt.query(user_query)
+    answer = sawserq_gpt.query(user_query)
 
     # send back the answer in json format
     data = {"answer": answer}
